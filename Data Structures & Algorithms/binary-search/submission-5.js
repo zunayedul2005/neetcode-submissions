8d04145f95de65c740ac1b7d2de5f4e928f2f7f7
zunayedul2005 <zunayedul2005@gmail.com>
2026-08-25 10:09:12 +0100

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let low =0
+        let high = nums.length-1
+
+        while(low<=high) {
+            let mid = Math.floor((high+low)/2);
+
+
+            if(nums[mid] ==target) return mid;
+
+           else if(nums[mid]< target) low = mid+1;
+
+            else high = mid-1;
+        }
+            return -1;
+    }
+}
