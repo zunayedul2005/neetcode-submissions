a290c56cfd173ca7bd35f3cb85acf26985af797a
zunayedul2005 <zunayedul2005@gmail.com>
2026-08-27 09:48:02 +0100

    Add: merge-two-sorted-linked-lists - submission-3

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-3.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} list1
+     * @param {ListNode} list2
+     * @return {ListNode}
+     */
+    mergeTwoLists(list1, list2) {
+        let dummy = new ListNode();
+        let node = dummy;
+        //early dge case check //optional
+
+        if(!list1 && !list2) return null;
+
+        while(list1 && list2) {
+            if(list1.val < list2.val){
+                node.next = list1;
+                list1 = list1.next;
+            }
+
+            else{
+                node.next = list2;
+                list2 = list2.next;
+            }
+
+            node = node.next;
+
+
+        }
+
+        node.next = list1 || list2;
+         
+        return dummy.next;
+    }
+}
+
+// i can add edge cases to exit early, without edge cases it works, too, since node.next chooses the valid one, if both are invalid, it attaches a null to its .next, returnign null when we do dummy.next
